bbbdf2e3228860a13ecc71ad81cf26a50aa80632
dieffenbachda26 <dieffenbachda26@vmi.edu>
2023-10-18 19:37:17 -0400

    +More minor fixes

--- mid/reg.php
+++ mid/reg.php
@@ -1,22 +1,22 @@
 <?php
 //TODO: Change connection.php file location later
-include("../frontend/connection.php");
+include("connection.php");
 
 var_dump($_POST);
 
-//TODO: Refactor variables to make consistent w/ camel case & get data processing down
+//TODO: Refactor variables to make consistent w/ camel case & get data processing down !!COMPLETED
 if(isset($_POST['Submit'])) {
     $fName = $_POST['fName'];
     $lName = $_POST['lName'];
-    $Email = $_POST['Email'];
-    $Phone = $_POST['Phone'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
 
-    $query = $connection->prepare("INSERT INTO `user` (`fName`, `lName`, `Email`, `Phone`) VALUES (:fName, :lName, :Email, :Phone)");
+    $query = $connection->prepare("INSERT INTO `user` (`fName`, `lName`, `email`, `phone`) VALUES (:fName, :lName, :email, :phone)");
 
     $query -> bindParam("fName", $fName, PDO::PARAM_STR);
     $query -> bindParam("lName", $lName, PDO::PARAM_STR);
-    $query -> bindParam("Email", $Email, PDO::PARAM_STR);
-    $query -> bindParam("Phone", $Phone, PDO::PARAM_STR);
+    $query -> bindParam("email", $email, PDO::PARAM_STR);
+    $query -> bindParam("phone", $phone, PDO::PARAM_STR);
 
     $result = $query->execute();
 }
