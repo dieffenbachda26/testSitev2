c6fd3d833c4311e39a0d3bf2ce1b7fc0c73b6d77
dieffenbachda26 <dieffenbachda26@vmi.edu>
2023-10-22 00:01:03 -0400

    +Worked on password field +Worked on email field
    !Fix email field error

--- mid/reg.php
+++ mid/reg.php
@@ -4,20 +4,24 @@ include("connection.php");
 
 var_dump($_POST);
 
-//TODO: Refactor variables to make consistent w/ camel case & get data processing down !!COMPLETED
 if(isset($_POST['Submit'])) {
     $fName = $_POST['fName'];
     $lName = $_POST['lName'];
     $email = $_POST['email'];
-    $phone = $_POST['phone'];
+    $pass = $_POST['pass'];
 
-    $query = $connection->prepare("INSERT INTO `user` (`fName`, `lName`, `email`, `phone`) VALUES (:fName, :lName, :email, :phone)");
+    $query = $connection->prepare("INSERT INTO `user` (`fName`, `lName`, `email`, `phone`, `pass`) VALUES (:fName, :lName, :email, :phone, :pass)");
 
     $query -> bindParam("fName", $fName, PDO::PARAM_STR);
     $query -> bindParam("lName", $lName, PDO::PARAM_STR);
     $query -> bindParam("email", $email, PDO::PARAM_STR);
     $query -> bindParam("phone", $phone, PDO::PARAM_STR);
+    $query -> bindParam("pass", $pass, PDO::PARAM_STR);
 
     $result = $query->execute();
 }
 ?>
+
+Figure this shit out : 
+array(6) { ["fName"]=> string(1) "j" ["lName"]=> string(1) "d" ["email"]=> string(8) "dingus@c" ["phone"]=> string(12) "434-434-4334" ["pass"]=> string(10) "dingUs51!@" ["Submit"]=> string(6) "Submit" }
+Fatal error: Uncaught PDOException: SQLSTATE[23000]: Integrity constraint violation: 1048 Column 'phone' cannot be null in C:\xampp\htdocs\testSitev2\mid\reg.php:21 Stack trace: #0 C:\xampp\htdocs\testSitev2\mid\reg.php(21): PDOStatement->execute() #1 {main} thrown in C:\xampp\htdocs\testSitev2\mid\reg.php on line 21
