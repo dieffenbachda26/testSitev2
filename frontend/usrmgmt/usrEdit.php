fee4caaf30a92feac00aa05e7add8c14425f8747
dieffenbachda26 <dieffenbachda26@vmi.edu>
2023-11-16 20:37:57 -0500

    !See checklist

--- frontend/usrmgmt/usrEdit.php
+++ frontend/usrmgmt/usrEdit.php
@@ -50,7 +50,8 @@
 </head>
 
 <!--fName, lName, email, phone, auth
-TODO: Add auth change if super admin-->
+TODO: Add auth change if super admin
+TODO: Regex for auth-->
 <div>
     <form action="../../mid/usrmgmt/usrEdit.php" method="post">
         <label for="fname">First name:</label>
@@ -61,19 +62,17 @@ TODO: Add auth change if super admin-->
 
         <!--TODO: Fix regex, allows things like t@t to pass-->
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" pattern="^[a-zA-Z0-9.-]+@[a-zA-Z]+[.][a-zA-Z]{3}$"
-            placeholder="<?= $result['email'] ?>"><br><br>
+        <input type="email" id="email" name="email" pattern="^[a-zA-Z0-9.-]+@[a-zA-Z]+[.][a-zA-Z]{3}$" placeholder="<?= $result['email'] ?>"><br><br>
 
         <label for="phone">Phone Number:</label>
-        <input type="text" id="phone" name="phone" pattern="(0-9)?\d{3}-\d{3}-\d{4}"
-            placeholder="<?= $result['phone'] ?>"><br><br>
+        <input type="text" id="phone" name="phone" pattern="^(0-9)?\d{3}-\d{3}-\d{4}$" placeholder="<?= $result['phone'] ?>"><br><br>
 
         <?php if ($_SESSION['auth'] == 2) { ?>
             <label for="auth">Privilege Level:</label>
-            <input type="text" id="auth" name="auth" placeholder="<?= $result['auth'] ?>"><br><br>
+            <input type="text" id="auth" name="auth" pattern="^[0-2]{1}$" placeholder="<?= $result['auth'] ?>"><br><br>
         <?php } ?>
 
-        <input type="submit" value="Edit" name="Edit">
+        <button type="submit" name="email" value="<?= $result['email'] ?>">Edit</button>
     </form>
 </div>
 
--- mid/usrmgmt/usrEdit.php
+++ mid/usrmgmt/usrEdit.php
@@ -4,19 +4,64 @@
     include("../../mid/connection.php");
 
     //Line below for debugging $_POST
-    var_dump($_POST);
+    //var_dump($_POST['fName']);
 
     $stmt = $connection->prepare('SELECT fName, lName, auth, email, phone FROM user WHERE email =:email');
     $stmt->bindParam('email', $_POST['email']);
-
     $stmt->execute();
-
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
     //Line below for debugging $result
     //var_dump($result);
 
     //TODO: Add auth change if super admin
+
+    //$query = $connection->prepare("INSERT INTO `user` (`fName`, `lName`, `email`, `phone`, `pass`, `auth`) VALUES (:fName, :lName, :email, :phone, :pass, :auth)");
+
+    //If first name is null, it equals default value; else set it to user entry
+    if($_POST['fName'] == null) {
+        $fName = $result['fName'];
+    } else {
+       $fName = $_POST['fName'];
+    }
+
+    if($_POST['lName'] == null) {
+        $fName = $result['lName'];
+    } else {
+       $fName = $_POST['lName'];
+    }
+
+    if($_POST['email'] == null) {
+        $fName = $result['email'];
+    } else {
+       $fName = $_POST['email'];
+    }
+
+    if($_POST['phone'] == null) {
+        $fName = $result['phone'];
+    } else {
+       $fName = $_POST['phone'];
+    }
+
+    if($_POST['auth'] == null) {
+        $fName = $result['auth'];
+    } else {
+       $fName = $_POST['auth'];
+    }
+
+    //Line below for debugging
+    var_dump($fName);
+
+    // $query->bindParam("fName", $fName, PDO::PARAM_STR);
+    //     $query->bindParam("lName", $lName, PDO::PARAM_STR);
+    //     $query->bindParam("email", $email, PDO::PARAM_STR);
+    //     $query->bindParam("phone", $phone, PDO::PARAM_STR);
+    //     $query->bindParam("pass", $hash, PDO::PARAM_STR);
+    //     $query->bindParam("auth", $auth, PDO::PARAM_STR);
+
+    //     $result = $query->execute();
+
+    //     header("Location: ../index.php");
 
 
 }
